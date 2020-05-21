051aecf55d41361207615fc316193e52c20319bb
NolanR <NolanR@server.utahs.contools-pg0.apt.emulab.net>
2020-05-20 18:09:14 -0600

    Added comments

--- client_setup.sh
+++ client_setup.sh
@@ -9,6 +9,16 @@ cd ~
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk python-pip
 sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+# Switch to compatible JDK version
+printf "\n*** IT IS OKAY TO RECEIVE \"error: no alternatives\" ERROR ***\n"
+printf "\n*** IF YOU RECEIVE \"directory does not exist: /usr/lib/jvm/java-1.8.0-openjdk-amd64\", READ COMMENTS IN THIS SCRIPT ***\n"
+# If failed, it is most likely due to the architecture of the machine you're using.
+# Go ahead and run "ls /usr/lib/jvm" and note the file that's prefixed with "java-1.8.0-openjdk-"
+# In the below line, adjust "amd64" to the system type that proceeds "java-1.8.0-openjdk-"
+# (e.g. arm64)
+# Then rerun this script
+
+echo "This should return \"openjdk version \"1.8.0_252\"\""
 java -version # This should output " openjdk version "1.8.0_252" "
 pip install cqlsh
 
@@ -19,6 +29,9 @@ echo "*** THIS HAS A CHANCE OF FAILING, IF IT DOES SEE COMMENTS IN THIS SCRIPT *
 # Then restart the script
 wget https://fossies.org/linux/misc/apache-cassandra-3.11.6-src.tar.gz
 tar -xzf apache-cassandra-3.11.6-src.tar.gz
+
+sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+java -version # This should output " openjdk version "1.8.0_252" "
 
 # We need this python lib
 rm -rf .local/lib/python2.7/site-packages/cqlshlib
